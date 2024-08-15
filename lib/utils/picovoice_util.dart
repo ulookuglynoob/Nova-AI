2eb481b3bda02259535860d09aa5ca37422d4ac3
R. Shah <ulookuglynoob@email.com>
2024-08-14 21:32:56 -0500

    Update picovoice_util.dart

--- lib/utils/picovoice_util.dart
+++ lib/utils/picovoice_util.dart
@@ -2,6 +2,7 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:porcupine_flutter/porcupine_manager.dart';
 import 'package:rhino_flutter/rhino_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PicovoiceUtil {
   final FlutterTts flutterTts;
@@ -17,53 +18,54 @@ class PicovoiceUtil {
       this.toggleTextRecognition, this.toggleImageDescription);
 
   Future<void> initPicovoice() async {
-  try {
-    _porcupineManager = await PorcupineManager.fromKeywordPaths(
-      "PICOVOICE-API-KEY",
-      ["assets/models/Hey-Nova_en_android_v3_0_0.ppn"],
-      (int keywordIndex) async {
-        if (keywordIndex == 0) {
-          await stopAll();
-          await flutterTts.speak("Yes?");
-          isListeningForCommand = true;
-          await _startRhinoListening();
-        }
-      },
-    );
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String apiKey = prefs.getString('picovoice_api_key') ?? 'default_api_key';
 
-    _rhinoManager = await RhinoManager.create(
-      "6yhnDSr6v3F6skMY93vu1lbowi60h7GrkaS5wbkaDrTNR5jllPfqFA==",
-      "assets/models/Nova_en_android_v3_0_0.rhn",
-      (inference) async {
-        if (isListeningForCommand) {
-          if (inference.isUnderstood ?? false) {
-            if (inference.intent == "objectDetection") {
-              await flutterTts.speak("Toggling object detection.");
-              toggleObjectDetection();
-            } else if (inference.intent == "textDetection") {
-              await flutterTts.speak("Toggling text detection.");
-              toggleTextRecognition();
-            } else if (inference.intent == "imageDescription") {
-              await flutterTts.speak("Toggling image description.");
-              toggleImageDescription();
-            }
-          } else {
-            await flutterTts.speak("Sorry, I didn't understand that.");
+    try {
+      _porcupineManager = await PorcupineManager.fromKeywordPaths(
+        apiKey,
+        ["assets/models/Hey-Nova_en_android_v3_0_0.ppn"],
+        (int keywordIndex) async {
+          if (keywordIndex == 0) {
+            await stopAll();
+            await flutterTts.speak("Yes?");
+            isListeningForCommand = true;
+            await _startRhinoListening();
           }
-          isListeningForCommand = false;
-        }
-      },
-    );
+        },
+      );
 
-    await _porcupineManager.start();
-  } catch (e) {
-    // Handle specific errors related to microphone initialization or other issues
-    toggleObjectDetection();
-    print('Error initializing Picovoice: $e');
-    await flutterTts.speak("An error occurred while initializing the microphone.");
+      _rhinoManager = await RhinoManager.create(
+        apiKey,
+        "assets/models/Nova_en_android_v3_0_0.rhn",
+        (inference) async {
+          if (isListeningForCommand) {
+            if (inference.isUnderstood ?? false) {
+              if (inference.intent == "objectDetection") {
+                await flutterTts.speak("Toggling object detection.");
+                toggleObjectDetection();
+              } else if (inference.intent == "textDetection") {
+                await flutterTts.speak("Toggling text detection.");
+                toggleTextRecognition();
+              } else if (inference.intent == "imageDescription") {
+                await flutterTts.speak("Toggling image description.");
+                toggleImageDescription();
+              }
+            } else {
+              await flutterTts.speak("Sorry, I didn't understand that.");
+            }
+            isListeningForCommand = false;
+          }
+        },
+      );
+
+      await _porcupineManager.start();
+    } catch (e) {
+      toggleObjectDetection();
+      print('Error initializing Picovoice: $e');
+      await flutterTts.speak("An error occurred while initializing the microphone.");
+    }
   }
-}
-
 
   Future<void> _startRhinoListening() async {
     await _rhinoManager.process();
